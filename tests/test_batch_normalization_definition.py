3169a705e38d5c51ec3b7773e01c05cbd8f330a6
“Wieger <j.w.wesselink@tue.nl>
2025-10-06 11:34:18 +0200

    Move batch normalization test to a separate file

---
+++ tests/test_batch_normalization_definition.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+# Copyright 2024 Wieger Wesselink.
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE or http://www.boost.org/LICENSE_1_0.txt)
+
+# see also https://docs.sympy.org/latest/modules/matrices/matrices.html
+
+from unittest import TestCase
+
+from nerva_sympy.matrix_operations import *
+
+from utilities import matrix
+
+Matrix = sp.Matrix
+
+
+class TestBatchNormalizationDefinition(TestCase):
+    def test_batch_norm_definition_section_3_1(self):
+        N = 4
+        D = 3
+        X = matrix('x', N, D)
+        R = X - (ones(N, N) / N) * X
+        Sigma = diag(R.T * R).T / N
+        mu = (ones(N).T * X) / N
+
+        self.assertTrue(R.shape == (N, D))
+        self.assertTrue(Sigma.shape == (1, D))
+        self.assertTrue(mu.shape == (1, D))
+
+        r = lambda i: X.row(i) - mu                        # r(i) is the i-th row of R
+        for i in range(N):
+            self.assertEqual(r(i), R.row(i))
+
+        sigma2 = lambda j: (R.col(j).T * R.col(j) / N)[0]  # sigma2(j) is the j-th element of Sigma
+        for j in range(D):
+            self.assertEqual(sigma2(j), Sigma[0, j])
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
